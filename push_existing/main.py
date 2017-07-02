c70e3968ad238e6c80c2babb8a78eb3c69965bb6
Spencer <spencer.michael.q@gmail.com>
2017-07-02 19:53:35 +0800

    syntax

--- push_existing/main.py
+++ push_existing/main.py
@@ -7,13 +7,16 @@ License: GNU AGPL, version 3 or later; https://www.gnu.org/licenses/agpl-3.0.en.
 """
 
 from aqt.qt import *
-
 from aqt import mw
-
 from anki.hooks import addHook
+from aqt.utils import showInfo
+
+__version__ = '1.0.0'
 
 ## NOTE: YOU MUST RESET THE SCHEDULER AFTER ANY DB CHANGES
 ## BY DOING mw.reset()
+
+HOTKEY = "Ctrl+Shift+P"
 
 ## Select the deck you'd want to change
 name_of_deck = ''
@@ -26,10 +29,23 @@ deck_note = deck.note()
 ## will search
 field_to_match = ''
 
+list_of_vocabs = []  # create an empty set, note: {} is an empty dictionary, set() = empty set
+
+
+def enter_vocab():
+    showInfo("hahahaha")
+
+
 for (name_of_field, contents) in deck_note.items():
     if name_of_field == field_to_match:
         ## change due value to 0
+        pass
 
-## I might need to use SQLITE
+## I might need to use SQLITE for changing the due value of the said cards
+## and unsuspend them simultaneously
 
+run_action = QAction("Push Existing Vocab", mw)
+run_action.setShortcut(QKeySequence(HOTKEY))
+mw.form.menuTools.addAction(run_action)
 
+run_action.triggered.connect(enter_vocab)
