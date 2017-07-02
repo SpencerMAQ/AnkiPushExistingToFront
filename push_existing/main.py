126efc98fa230c26d6e4a7a4cba79e9f8fe82586
Spencer <spencer.michael.q@gmail.com>
2017-07-02 16:19:30 +0800

    .

--- push_existing/main.py
+++ push_existing/main.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 """
-This file is part of the Cloze Overlapper add-on for Anki
-Main Module, hooks add-on methods into Anki
-Copyright: Glutanimate 2016-2017
+This file is part of the Vocab Pusher add-on for Anki
+Copyright: SpencerMAQ 2016-2017
 License: GNU AGPL, version 3 or later; https://www.gnu.org/licenses/agpl-3.0.en.html
 """
+
+from aqt.qt import *
+
+from aqt import mw
 
