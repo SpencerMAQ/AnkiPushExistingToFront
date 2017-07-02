17d73edcac7b3d13ec17b1847ad90fad7ebcd7e2
Spencer <spencer.michael.q@gmail.com>
2017-07-02 16:12:50 +0800

    first

---
+++ Push_Existing_Vocab.py
@@ -0,0 +1,6 @@
+# if the card exists and is already due, do not chat its due
+
+# if the card exists and is suspended and due, create notification
+
+
+## Generate General notification of the cards that were modified, what happened
