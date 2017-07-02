16fa72d9c6b38dc59c5f9c1f081f15030e034e7f
Spencer <spencer.michael.q@gmail.com>
2017-07-02 16:21:14 +0800

    .

--- Push_Existing_Vocab.py
+++ Push_Existing_Vocab.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+Anki Add-on: Push Existing Vocab
+Copyright: (c) SpencerMAQ 2016-2017
+License: GNU AGPL, version 3 or later; https://www.gnu.org/licenses/agpl-3.0.en.html
+"""
+
 # possibilities:
 # suspended, due, new, learn, review, buried
 # if the card exists and is already due, do not chat its due
@@ -6,3 +14,5 @@
 
 
 ## Generate General notification of the cards that were modified, what happened
+
+import push_existing.main
