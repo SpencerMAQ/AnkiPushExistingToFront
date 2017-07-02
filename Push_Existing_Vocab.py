f3f59d2db0d0991bfa7eb938ab86d08d043fc7ee
Spencer <spencer.michael.q@gmail.com>
2017-07-02 16:18:24 +0800

    .

--- Push_Existing_Vocab.py
+++ Push_Existing_Vocab.py
@@ -1,3 +1,5 @@
+# possibilities:
+# suspended, due, new, learn, review, buried
 # if the card exists and is already due, do not chat its due
 
 # if the card exists and is suspended and due, create notification
---
+++ push_existing/main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+This file is part of the Cloze Overlapper add-on for Anki
+Main Module, hooks add-on methods into Anki
+Copyright: Glutanimate 2016-2017
+License: GNU AGPL, version 3 or later; https://www.gnu.org/licenses/agpl-3.0.en.html
+"""
+
