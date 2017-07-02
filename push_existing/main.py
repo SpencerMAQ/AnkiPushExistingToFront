afb233628a78150f0f91425f663ca57f0c34335f
Spencer <spencer.michael.q@gmail.com>
2017-07-02 16:45:42 +0800

    .

--- push_existing/main.py
+++ push_existing/main.py
@@ -10,3 +10,17 @@ from aqt.qt import *
 
 from aqt import mw
 
+## NOTE: YOU MUST RESET THE SCHEDULER AFTER ANY DB CHANGES
+## BY DOING mw.reset()
+
+## Select the deck you'd want to change
+deck_to_change = ''
+deck_id = mw.col.decks.id(deck_to_change)
+mw.col.decks.select(deck_id)
+
+## The name of the field where the add-on
+## will search
+field_to_match = ''
+
+
+
