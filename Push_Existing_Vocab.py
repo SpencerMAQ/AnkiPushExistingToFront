e89e990c810d13ecdee02e15e340043940dfd10b
Michael Spencer Quinto <SpencerMAQ@users.noreply.github.com>
2018-01-18 19:16:21 +0800

    update license yr

--- Push_Existing_Vocab.py
+++ Push_Existing_Vocab.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Anki Add-on: Push Existing Vocab
-# Copyright: SpencerMAQ (Michael Spencer Quinto) <spencer.michael.q@gmail.com> 2017
+# Copyright: SpencerMAQ (Michael Spencer Quinto) <spencer.michael.q@gmail.com> 2017-2018
 # License: GNU AGPL, version 3 or later; https://www.gnu.org/licenses/agpl-3.0.en.html
 
 # Creates a log file called logging.log on ~/Documents/Anki/addons/push/existing
