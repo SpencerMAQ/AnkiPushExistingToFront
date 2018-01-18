25ed1b492270154fd0a0d4d07141a579a4aa19f2
Michael Spencer Quinto <SpencerMAQ@users.noreply.github.com>
2018-01-18 19:17:15 +0800

    Update utils.py

--- push_existing/utils.py
+++ push_existing/utils.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 # This file is part of the Push Existing Vocab add-on for Anki
-# Copyright: SpencerMAQ (Michael Spencer Quinto) <spencer.michael.q@gmail.com> 2017
+# Copyright: SpencerMAQ (Michael Spencer Quinto) <spencer.michael.q@gmail.com> 2017-2018
 # License: GNU AGPL, version 3 or later; https://www.gnu.org/licenses/agpl-3.0.en.html
+
 from aqt.addons import AddonManager
 from aqt import mw
 import logging
